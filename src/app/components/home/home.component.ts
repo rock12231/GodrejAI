a53a34d6635115700027ee0efb4f80dca73bb7e2
Avinash Kumar <37548802+rock12231@users.noreply.github.com>
2024-09-22 23:15:07 +0530

    Home Fix

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FirebaseService } from '../../shared/services/firebase.service';
+import { Database, push, ref } from '@angular/fire/database';
+import { ToastAlertService } from '../../shared/services/toast-alert.service';
+import { SpinnerService } from '../../shared/services/spinner.service';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +12,33 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  user: any;
+
+  constructor(
+    public fauth: FirebaseService,
+    public db: Database,
+    private toastService: ToastAlertService,
+    private spinnerService: SpinnerService
+  ) {
+    if (typeof localStorage !== 'undefined' && localStorage.getItem('user')) {
+      this.user = localStorage.getItem('user');
+      this.user = JSON.parse(this.user);
+    }
+  }
+
+  async contactUs(name: string, email: string, message: string) {
+    this.spinnerService.show();
+    const userRef = ref(this.db, `users/contact`);
+    await push(userRef, { name, email, message })
+      .then(() => {
+        this.spinnerService.hide();
+        this.toastService.showToast('Message sent successfully', 'success', 'top-end');
+      })
+      .catch((error) => {
+        this.spinnerService.hide();
+        this.toastService.showToast('Error sending message', 'error', 'top-end');
+      });
+  }
 
 }
