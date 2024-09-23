19a3660f5dc1e772b3c1bfc681cfa0708fcb4df1
Avinash Kumar <37548802+rock12231@users.noreply.github.com>
2024-09-23 16:00:37 +0530

    chat save to db

--- src/app/components/chat-ai/chat-ai.component.ts
+++ src/app/components/chat-ai/chat-ai.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { FirebaseService } from '../../shared/services/firebase.service';
 import { Router } from '@angular/router';
-import { Database, get, ref } from '@angular/fire/database';
+import { Database, get, push, ref, set } from '@angular/fire/database';
 import { ToastAlertService } from '../../shared/services/toast-alert.service';
 import { SpinnerService } from '../../shared/services/spinner.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ApiService } from '../../shared/services/api.service';
 
 @Component({
   selector: 'app-chat-ai',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './chat-ai.component.html',
   styleUrl: './chat-ai.component.css'
 })
@@ -19,13 +20,15 @@ export class ChatAIComponent {
   user: any;
   chats: any;
   activeSection: string = 'AI Chat';
+  result: any;
 
   constructor(
     public fauth: FirebaseService,
     private router: Router,
     public db: Database,
     private toastService: ToastAlertService,
-    private spinnerService: SpinnerService
+    private spinnerService: SpinnerService,
+    private apiService: ApiService
   ) {
     if (typeof localStorage !== 'undefined' && localStorage.getItem('user')) {
       this.user = localStorage.getItem('user');
@@ -37,7 +40,7 @@ export class ChatAIComponent {
     const userRef = ref(this.db, `users/${this.user.uid}/info`);
     const userSnapshot = await get(userRef);
     const profile = userSnapshot.val()
-    if(profile.department == undefined || profile.interests == undefined || profile.skills == undefined){
+    if (profile.department == undefined || profile.interests == undefined || profile.skills == undefined) {
       this.toastService.showToast('Please complete your profile first', 'error', 'top-end');
       this.router.navigate(['profile', this.user.displayName]);
     }
@@ -48,7 +51,7 @@ export class ChatAIComponent {
     this.activeSection = section;
   }
 
-  async getChat(){
+  async getChat() {
     const chatRef = ref(this.db, `users/${this.user.uid}/chat`);
     const chatSnapshot = await get(chatRef);
     const chat = chatSnapshot.val()
@@ -59,4 +62,50 @@ export class ChatAIComponent {
     }));
     this.chats = chatArray;
   }
+
+  async sendMessage(prompt: any) {
+    this.spinnerService.show();
+    try {
+      const userRef = ref(this.db, `users/${this.user.uid}/info`);
+      const snapshot = await get(userRef);
+      const user_data = snapshot.val();
+      const response$ = await this.apiService.generate(prompt, user_data);
+      response$.subscribe(
+        response => {
+          this.spinnerService.hide();
+          console.log('AI Response:', response);
+          this.result = response;
+          if (response.choices.length > 0) {
+            this.saveChat(prompt, response.choices[0].text);
+          }
+        },
+        error => {
+          this.spinnerService.hide();
+          console.error('Error generating response:', error);
+        }
+      );
+    } catch (error) {
+      this.spinnerService.hide();
+      console.error('Error retrieving auth token:', error);
+    }
+  }
+
+  async saveChat(question: string, response: string) {
+    const chatRef = ref(this.db, `users/${this.user.uid}/chat`);
+    const createdAt = new Date().toISOString();
+    const chatData = {
+      [createdAt]: {
+        question,
+        response
+      }
+    };
+    await set(chatRef, chatData)
+      .then(() => {
+        this.getChat();
+      })
+      .catch((error) => {
+        console.error('Error saving chat:', error);
+      });
+  }
+
 }
--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { FirebaseService } from './firebase.service';
 
 @Injectable({
@@ -8,16 +8,32 @@ import { FirebaseService } from './firebase.service';
 })
 export class ApiService {
 
-  private flaskApiUrl = 'http://127.0.0.1:5000/test';
+  private baseUrl = 'https://godreja.onrender.com';
 
-  constructor(private http: HttpClient, private fauth: FirebaseService) {}
+  constructor(
+    private http: HttpClient, 
+    private fauth: FirebaseService
+  ) { }
 
-  async postData(data: any): Promise<Observable<any>> {
-    const token = await this.fauth.getAuthToken();
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${token}`,
-    });
-
-    return this.http.post(this.flaskApiUrl, data, { headers });
+  async generate(prompt: string, user_data:any): Promise<Observable<any>> {
+    const url = `${this.baseUrl}/generate`;
+    const body = { prompt, user_data };
+    try {
+      const token = await this.fauth.getAuthToken()
+      const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      });
+      return this.http.post(url, body, { headers }).pipe(
+        catchError(this.handleError)
+      );
+    } catch (error) {
+      return throwError(error);
+    }
+  }
+  // Handle API errors
+  private handleError(error: any): Observable<never> {
+    console.error('An error occurred:', error);
+    return throwError(error);
   }
 }
